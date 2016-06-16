5895f34213dd699c17cb7817b743a19c052a8586
xiaoqi05 <772508341@qq.com>
2016-06-16 11:33:21 +0800

    add user get set method

--- src/com/twu/biblioteca/model/User.java
+++ src/com/twu/biblioteca/model/User.java
@@ -1,12 +1,67 @@
 package com.twu.biblioteca.model;
 
 public class User {
-    //(name, email address and phone number
     private String name;
+    private String password;
     private String email;
     private String address;
     private String libraryNumber;
     private int phoneNumber;
 
+    public User(String name, String password, String email, String address, String libraryNumber, int phoneNumber) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.address = address;
+        this.libraryNumber = libraryNumber;
+        this.phoneNumber = phoneNumber;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLibraryNumber() {
+        return libraryNumber;
+    }
+
+    public void setLibraryNumber(String libraryNumber) {
+        this.libraryNumber = libraryNumber;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
